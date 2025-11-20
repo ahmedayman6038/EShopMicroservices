1a950c4ee0a11a3f9355426299f4f7eab8370440
Ahmed Hammad <Ahmed.Hammad@linkdev.com>
2025-11-20 18:40:06 +0200

    finish section 6

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using Catalog.API.Products.GetProductById;
+using JasperFx.Events.Daemon;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling DeleteProductCommand {@Command}", command);
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+        }
+    }
+}
